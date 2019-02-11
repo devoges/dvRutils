b3f2d4d3f07368178891a16d4d9a5aa6fceade59
devoges <ndevogel@mcw.edu>
2019-02-11 14:59:15 -0600

    fix load many

--- R/general.R
+++ R/general.R
@@ -65,9 +65,52 @@ nlapply <- function(n, ...) {
 #' @export
 #'
 #' @examples
-library_many <- function(x) {
-  lapply(x, library, character.only = TRUE)
+library_many <- function(...) {
+  packages <- as.character(match.call(expand.dots = FALSE)[[2]])
+
+  try <- unlist(lapply(packages, function(package) {
+    suppressWarnings(require(package, character.only = TRUE, quietly = TRUE))
+  }))
+  names(try) <- packages
+
+  good <- names(which(try))
+  bad <- names(which(!try))
+
+  try_install <- unlist(lapply(bad, function(package) {
+    suppressWarnings(install.packages(package,
+                                      dependencies = TRUE))
+    ok <- suppressWarnings(require(package,
+                                   character.only = TRUE,
+                                   quietly = TRUE))
+
+    if ( !ok ) {
+      #message(paste(package, "not found on CRAN. Trying Bioconductor"))
+
+      if ( !require("BiocManager", quietly = TRUE) ) {
+        install.packages("BiocManager")
+      }
+
+      suppressWarnings(BiocManager::install(package,
+                                            update = FALSE,
+                                            ask = FALSE,
+                                            dependencies = TRUE))
+    }
+
+    suppressWarnings(require(package, character.only = TRUE, quietly = TRUE))
+  }))
+  names(try_install) <- bad
+
+  if ( length(good) > 0 )  message(paste("Loaded:", paste(good, collapse = ", ")))
+  if ()
+  message(paste("Loaded (and installed from CRAN):", paste(good, collapse = ", ")))
+  message(paste("Loaded (and installed from BioConductor):", paste(names(which(try_install)), collapse = ", ")))
+  message(paste("NOT Loaded (can't find on CRAN or BioConductor):", paste(names(which(!try_install)), collapse = ", ")))
 }
+
+packages <- c("dplyr", "magrittr", "sSeq", "tacobell")
+
+debug(library_many)
+library_many(dplyr, magrittr, sSeq, tacobell)
 
 #' clear
 #'
